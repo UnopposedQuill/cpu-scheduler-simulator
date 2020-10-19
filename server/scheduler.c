e354fd5ca14d1afc2f1a8a98c4c68dcf0eee4ff7
UnopposedQuill <darklightbeing@gmail.com>
2020-10-18 19:32:23 -0600

    Changes to thread data to read configurations

--- server/scheduler.c
+++ server/scheduler.c
@@ -3,6 +3,7 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
 
 #include "scheduler.h"
 #include "pcb.h"
@@ -16,13 +17,23 @@ void * jobSchedulerWork(void * arguments){
 
 void * cpuSchedulerWork(void * arguments){
     printf("Removing test jobs\n");
-    struct pcbList * readyList = (struct pcbList *) arguments;
-    struct pcbNode * removedNode = removePcbPid(readyList, 10);
+    struct schedulerInfo * _schedulerInfo = (struct schedulerInfo *) arguments;
+    struct pcbNode * removedNode = removePcbPid(_schedulerInfo->readyList, 10);
     printf("Removed node pid: %d\n", removedNode->node->pid);
+    sleep(_schedulerInfo->_configuration.schedulerType == FIFO ? 10 : 5);
     return NULL;
 }
 
 struct pcbNode * schedule(struct schedulerInfo _schedulerInfo){
 
+    /**
+     * I'll employ a strategy pattern here, depending on the value of the scheduler type
+     */
+    switch (_schedulerInfo._configuration.schedulerType) {
+        case FIFO:{
+            return _schedulerInfo.readyList->firstNode;
+        }
+
+    }
     return NULL;
 }
